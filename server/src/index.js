96fe227344887f8a4f41acedd2b30bf9864622be
Jagrati3 <jagratikhorwal800@gmail.com>
2025-03-08 18:00:47 +0530

    adding all backend file to Github

--- server/src/index.js
+++ server/src/index.js
@@ -12,13 +12,16 @@ app.use(cors());
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
+// Add this line to remove the warning
+mongoose.set('strictQuery', false);
+
 mongoose.connect(
   "mongodb+srv://Jagrati:UX6KWNW-G7V84aU@cluster0.mcl0x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).then(
-    console.log("DB connected...")
+    console.log("DB connected sucessfully ...")
   )
 
-app.listen(3001, () => console.log("Server started"));
+app.listen(3001, () => console.log("Server started successfully!!!"));
